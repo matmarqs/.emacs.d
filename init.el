1e023f7aad21d8743a3ddc110f8ecb30b37ff59a
Mateus Marques <marqs.mts@gmail.com>
2026-08-26 15:56:40 -0300

    tmux passthrough C-\

--- init.el
+++ init.el
@@ -118,6 +118,7 @@
 ;; ============================================================
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-unset-key (kbd "C-\\")) ; Pass through to tmux prefix in ghostel
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C-_") 'text-scale-decrease)
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
